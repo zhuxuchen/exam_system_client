8d01536281436160634e62e40b6088f49aa3ca0c
zhuxuchen <2517870679@qq.com>
2022-11-05 20:56:06 +0800

    修改了路由跳转方式

--- src/views/StudentIndex.vue
+++ src/views/StudentIndex.vue
@@ -6,8 +6,8 @@
           <li class="logo" @click="exam()"><i class="iconfont icon-kaoshi"></i><span>Exam-Online</span></li>
           <li><a href="javascript:;" @click="exam()">我的试卷</a></li>
           <li><a href="javascript:;" @click="practice()">我的练习</a></li>
-          <li><router-link to="/scoreTable">我的分数</router-link></li>
-          <li><router-link to="/message">给我留言</router-link></li>
+          <li><router-link to="/scoreTable" replace>我的分数</router-link></li>
+          <li><router-link to="/message" replace>给我留言</router-link></li>
           <li><a href="javascript:;">待定</a></li>
           <li class="right" @mouseenter="flag = !flag" @mouseleave="flag = !flag">
             <a href="javascript:;"><i class="iconfont icon-Userselect icon"></i>{{user.userName}}</a>
